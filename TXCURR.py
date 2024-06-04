16e655ee230031e0f2c14d30f8a5ef6ad1ce2649
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-06-04 08:00:34 -0700

    Update TXCURR.py

--- TXCURR.py
+++ TXCURR.py
@@ -206,7 +206,9 @@ if file is not None:
             
             df[['Tyear', 'Ryear', 'Rmonth', 'Rday', 'Vyear', 'Vmonth', 'Ayear']] = df[['Tyear', 'Ryear', 'Rmonth', 'Rday', 'Vyear', 'Vmonth', 'Ayear']].apply(pd.to_numeric, errors='coerce')
             dfw = df[df['Ryear'] ==2025].copy()
-            dfy = df[((df['Ryear'] ==2024)  & (df['Rmonth']>3))].copy()
+            dfy = df[df['Ryear'] ==2024].copy()
+            dfy['Rmonth', 'Rday'] = dfy['Rmonth', 'Rday'].apply(pd.to_numeric, errors = 'coerce')
+            dfy = dfy[((dfy['Rmonth']>3) | ((dfy['Rmonth']==3) & (dfy['Rday'] >3)))].copy()
             df = pd.concat([dfw,dfy])
             potential = df.shape[0]
             df[['Tyear', 'Ryear', 'Rmonth', 'Rday', 'Vyear', 'Vmonth', 'Ayear']] = df[['Tyear', 'Ryear', 'Rmonth', 'Rday', 'Vyear', 'Vmonth', 'Ayear']].apply(pd.to_numeric, errors='coerce')
