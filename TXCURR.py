78370dfadb997f27813c03e08478713b4e0b141f
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-06-07 00:16:36 -0700

    Update TXCURR.py

--- TXCURR.py
+++ TXCURR.py
@@ -175,10 +175,10 @@ if file is not None:
             dfa = df.shape[0]
 
             # #SORTING THE TRANSFER IN DATE YEARS
-            df['Tiyear'] = df['Tiyear'].fillna(2022)
-            df['Timonth'] = df['Timonth'].fillna(2)
-            df['Tiday'] = df['Tiday'].fillna(2)
-            df[['Tiyear', 'Tiday']] =df[['Tiyear','Tiday']].apply(pd.to_numeric, errors = 'coerce')
+            #df['Tiyear'] = df['Tiyear'].fillna(2022)
+            #df['Timonth'] = df['Timonth'].fillna(2)
+            #df['Tiday'] = df['Tiday'].fillna(2)
+            #df[['Tiyear', 'Tiday']] =df[['Tiyear','Tiday']].apply(pd.to_numeric, errors = 'coerce')
             df['Tiyear'] = df['Tiyear'].fillna(2022)
             a = df[df['Tiyear']>31].copy()
             b = df[df['Tiyear']<32].copy()
