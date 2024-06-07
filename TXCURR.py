95cf750e38d899224a2a9c9dcbd018235e01d44b
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-06-06 23:52:57 -0700

    Update TXCURR.py

--- TXCURR.py
+++ TXCURR.py
@@ -82,6 +82,7 @@ if file is not None:
             df['VD'] = df['VD'].str.replace('00:00:00', '', regex=True)
             df['TO'] = df['TO'].str.replace('00:00:00', '', regex=True)
             df['TI'] = df['TI'].str.replace('00:00:00', '',regex=True)
+            df['TI'] = df['TI'].fillna(2000)
             try:
                 df[['Ayear', 'Amonth', 'Aday']] = df['AS'].str.split('*', expand = True)
             except:
