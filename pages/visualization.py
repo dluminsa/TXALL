2f56e51c4faacfc109d7f1f5d5187b757d199ff0
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-06-08 14:29:10 -0700

    Update visualization.py

--- pages/visualization.py
+++ pages/visualization.py
@@ -203,7 +203,7 @@ st.plotly_chart(fig5, use_container_width= True)
 st.divider()
 current_time = time.localtime()
 k = time.strftime("%V", current_time)
-k= 24
+#k= 24
 k = int(k)
 m = k-1
 highest = filtered_df[filtered_df['TXML']>100]
@@ -228,6 +228,9 @@ else:
      )
     with coly:
          st.plotly_chart(figa, use_container_width=True)
+         st.markdown('##')
+         with st.expander('HIGHEST TXML'):
+                 st.table(highest)
 if highestb.shape[0]==0:
     with colu:
          st.markdown('##')
@@ -247,9 +250,7 @@ else:
         highest = highest[['FACILITY', 'TXML']]
         highest. set_index('FACILITY', inplace= True)
         highest['TXML'] = highest['TXML'].astype(int)
-        st.markdown('##')
-        with st.expander('HIGHEST TXML'):
-                 st.table(highest)
+
 #############################################################################################
 st.divider()
 col1, col2,col3 = st.columns(3)
@@ -276,11 +277,17 @@ with col1:
      poorvl.set_index('DISTRICT', inplace=True)
      with st.expander('FACILITIES WITH POOR VL COV'):
          st.dataframe(poorvl)
-with col2:
-    pied = filtered_df[['HAS VL', 'NO VL']]
-    melted = pied.melt(var_name='Category', value_name='values')
-    fig = px.pie(melted, values= 'values', names='Category', hole=0.5)
+with col2
+pied = filtered_df[filtered_df['WEEK']==k]    
+pied = pied[['HAS VL', 'NO VL']]
+melted = pied.melt(var_name='Category', value_name='values')
+fig = px.pie(melted, values= 'values', names='Category', hole=0.5)
     #fig.update_traces(text = 'VL COVERAGE', text_position='Outside')
+if pied.shape[0]==0:
+    st.markdown('##')
+    st.markdown('##')
+    st.write("Selected facility or facilities didn't report this week, so nothing to show")
+else:
     st.plotly_chart(fig, use_container_width=True)
 
 st.divider()
