29b27bcce2edfed823a85adc33285815613e54b3
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-06-09 04:18:48 -0700

    Update TXCURR.py

--- TXCURR.py
+++ TXCURR.py
@@ -112,7 +112,8 @@ if file is not None:
                 df[['Ryear', 'Rmonth', 'Rday']] = df['RD'].str.split('*', expand = True)
             except:
                 pass
-
+           st.write(df['RD'].head(5))
+           st.stop()
             try:
                 #df['RD'] = df['RD'].astype(str)
                 df['RD'] = pd.to_numeric(df['RD'], errors='coerce')
